1f057223aa2431fd8cd60e1ed12bd00972265145
Philip Watson <pheee0@gmail.com>
2022-01-22 00:24:05 -0700

    Clean up project, add readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
+const mainApi = "https://pokeapi.co/api/v2/pokemon/";
+const maxId = 898;
+const randomNumber = Math.floor(Math.random() * maxId + 1);
 function App() {
+  const [pokemon, setPokemon] = useState(null);
+
+  console.log(randomNumber);
+  console.log(mainApi + randomNumber);
+
+  async function fetchUserData(id) {
+    const response = await fetch(mainApi + id);
+    setPokemon(await response.json());
+  }
+
+  useEffect(() => {
+    fetchUserData(randomNumber);
+  }, []);
+
+  if (!pokemon) {
+    return "loading...";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="box" id="heading">
+        <h1> A Random Pokemon </h1>
+      </div>
+
+      <div class="card">
+        <img
+          src={pokemon.sprites.front_default}
+          class="card-img-top"
+          alt={pokemon.name}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{pokemon.name}</h5>
+          <p class="card-text"></p>
+        </div>
+      </div>
     </div>
   );
 }
